7835ea71eb25ba70e91b53733ea05a93e2dc6b9f
Arnaud <arnaud_pesenti@yahoo.fr>
2024-06-20 16:57:30 +0200

    Update of 'parse_movie' which should be complete. Implementation of 'parse_casting' method --complete--. To do: scrapy 'item' implementation + data cleaning with scrapy pipelines.

--- Movies/spiders/movies_spider.py
+++ Movies/spiders/movies_spider.py
@@ -1,4 +1,5 @@
 import scrapy
+from urllib.parse import urljoin
 
 
 class MoviesSpiderSpider(scrapy.Spider):
@@ -8,50 +9,34 @@ class MoviesSpiderSpider(scrapy.Spider):
     allowed_domains = ["allocine.fr"]
 
     def parse(self, response):
-        """Navigates between movies pages"""
+        """
+        Navigates between movies pages
+        """
 
         # BASIC SETTINGS & INITIALIZATION
         stop = False
         movies = response.xpath("//li[@class='mdl']")
         self.n = 0 if not hasattr(self, 'n') else self.n
 
-        # print('##################################################')
-        # print(f"LONGUEUR DE 'movies'{len(movies)}")
-        # print('##################################################')
-
         # EXPLORES EACH MOVIE PAGE & RETRIEVES RELATED DATA
         for movie in movies:
             self.n += 1
             movie_url = movie.xpath('.//h2/a/@href').get()
-            # print('##################################################')
-            # print(movie_url)
-            # print('##################################################')
-            # # print(type(movie_url))
-            # # print('##################################################')
-            # # movie_url = movie_url[5:]
-            # # print(movie_url)
-            # # print('##################################################')
 
             if self.limit and self.limit <= self.n:
                 stop = True
-                print("#####################################################")
-                print(F"{self.limit}/{self.n} -- BREAK")
-                print("#####################################################")
                 break
             else:
-                print("#####################################################")
-                print("MOUCHARD DE TEST")
-                print("#####################################################")
                 yield response.follow(movie_url, self.parse_movie)
 
-        # LOOKS FOR A POTENTIAL NEW PAGE TO EXPLORE
+        # LOOKS FOR A POTENTIAL NEW PAGE TO EXPLORE AND MOVE TO IT IF ANY
         next_page = self.get_next_page(response)
         if next_page and not stop:
             yield response.follow(next_page, callback=self.parse)
 
 
     def get_next_page(self, response):
-        """Returns the new page url to follow or None"""
+        """Returns the new page url to follow or none"""
 
         # BASIC SETTINGS & INITIALIZATION
         url = None
@@ -77,11 +62,13 @@ class MoviesSpiderSpider(scrapy.Spider):
 
     def parse_movie(self, response):
         """
-            Parse a movie page to retrieve related data (title, synopsis, etc.)
+        Parse a movie page to retrieve related data (title, synopsis, etc.)
         """
 
-        # BASIC SETTINGS & INITIALIZATION
+        # BASIC SETTINGS & INITIALIZATION OR FEATURES
+        grab = lambda x: '¤'.join(response.xpath(x).getall())
         meta = "//div[contains(@class, 'card') and contains(@class, 'entity')]"
+        casting_url = grab("//a[contains(@title, 'Casting')]/@href")
 
         # IMPLEMENTATING DATA PATHS FOR PURELY TEXT VALUES
         paths = {
@@ -96,10 +83,31 @@ class MoviesSpiderSpider(scrapy.Spider):
         # IMPLEMENTING DATA PATHS FOR TAG ATTRIBUTES
         attributes = {'MoviePoster': f"{meta}//figure//img/@src"}
 
-        # DATA SCRAPING
-        grab = lambda x: '¤'.join(response.xpath(x).getall())
+        # RETRIEVING MOVIE GENERAL DATA
         data = {key: grab(f'{path}/text()') for key, path in paths.items()}
         data.update({key: grab(path) for key, path in attributes.items()})
 
+        # RETRIEVING MOVIE CASTING DATA IF AVAILABLE
+        if not casting_url:
+            yield data
+        else:
+            casting_url = urljoin(response.url, casting_url)
+            yield scrapy.Request(url=casting_url,
+                                 meta={'data': data},
+                                 callback=self.parse_casting)
+
+
+    def parse_casting(self, response):
+        """
+        Parse the cast page to retrieve casting data.
+        """
+
+        # RETRIEVES CASTING DATA
+        path = "//section[contains(@class, 'actor')]"
+        casting = "¤".join(response.xpath(path).getall())
+
+        # UPDATES MOVIE DATA WITH ITS CASTING DATA
+        response.meta['data'].update({'Casting': casting})
+
         # FUNCTION OUTPUT
-        yield data
+        yield response.meta['data']
