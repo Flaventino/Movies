2ca35876451dda67ecb9929cb8d3f75e4ca459a3
Arnaud <arnaud_pesenti@yahoo.fr>
2024-07-01 15:04:46 +0200

    'Databases' directory update: Started implementing the database's schema via SQLAlchemy

--- databases/schema.py
+++ databases/schema.py
@@ -1,3 +1,25 @@
+from sqlalchemy import CheckConstraint as SetRule
 from sqlalchemy import Column, Integer, String, create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
+# INSTANCIATING A VIRTUAL SHAPE (OR SCTRUCTURE) FOR THE DATABASE 
+MovieDB = declarative_base()
+
+# CREATING TABLES (within the database)
+class movie(MovieDB):
+    # RAW PARAMETERS AND SETINGS
+    __tablename__ = 'movie'
+
+    # COLUMNS OF THE TABLE
+    Id = Column(Integer, primary_key=True, autoincrement=True)
+    Title = Column(String, nullable=True)
+    Title_Fr = Column(String, nullable=True)
+    Synopsis = Column(String, nullable=True)
+
+
+    # SCHEMA CONSTRAINTS
+    rule1 = SetRule(name="At_least_one_title_required",
+                    sqltext="title IS NOT NULL OR title_fr IS NOT NULL")
+
+    __table_args__ = (rule1)
+    pass
