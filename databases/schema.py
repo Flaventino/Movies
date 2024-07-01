17b28da6ce9fab790fbc56ab8d6dd08fc4e51b73
Arnaud <arnaud_pesenti@yahoo.fr>
2024-07-01 16:53:47 +0200

    Database inmplementation: 'movie' table is under development. Added some relevant fields (columns) and their types

--- databases/schema.py
+++ databases/schema.py
@@ -1,5 +1,5 @@
 from sqlalchemy import CheckConstraint as SetRule
-from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy import Column, Integer, String, Date, create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 # INSTANCIATING A VIRTUAL SHAPE (OR SCTRUCTURE) FOR THE DATABASE 
@@ -10,16 +10,24 @@ class movie(MovieDB):
     # RAW PARAMETERS AND SETINGS
     __tablename__ = 'movie'
 
+
     # COLUMNS OF THE TABLE
     Id = Column(Integer, primary_key=True, autoincrement=True)
     Title = Column(String, nullable=True)
     Title_Fr = Column(String, nullable=True)
     Synopsis = Column(String, nullable=True)
+    Duration = Column(Integer, nullable=True)
+    Poster_url = Column(String, nullable=True)
+    Release_date = Column(Date, nullable=True)
+    Release_place = Column(String, nullable=True)
+    Production_year = Column(String, nullable=True)
+    Movie_visa_number = Column(String, nullable=True)
 
+    
 
     # SCHEMA CONSTRAINTS
-    rule1 = SetRule(name="At_least_one_title_required",
+    rule = SetRule(name="At_least_one_title_required",
                     sqltext="title IS NOT NULL OR title_fr IS NOT NULL")
 
-    __table_args__ = (rule1)
+    __table_args__ = (rule)
     pass
